5b559156c4a2242679841fa9113c0fe845ae67e2
zaiaaa <gustavozaia75@gmail.com>
2025-02-25 22:29:14 -0300

    iniciando com variáveis, print, scan e operador aritimético. Atividade simples onde se informam só 2 números, e posteriormente eles são multiplicados.

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,16 @@
 # include <stdio.h>
-//swadawddwawdadwadw
+
 int main(){
-    printf("Hello World!");
+    int n1, n2, res;
+    
+    printf("Digite o número 1: \n");
+    scanf("%i", &n1);
+    
+    printf("Digite o número 2: \n");
+    scanf("%i", &n2);
+    
+    res = n1 * n2;
+    
+    printf("Resultado: %i", res);
     return 0;
 }
