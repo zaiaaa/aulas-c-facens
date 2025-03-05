510f3401b13ad8f1bea8a02fc84ec53434faea63
zaiaaa <gustavozaia75@gmail.com>
2025-03-05 20:33:59 -0300

    entendendo mais coisas e fazendo C rodar no meu pc

--- hello_world.c
+++ hello_world.c
@@ -4,13 +4,15 @@ int main(){
     int n1, n2, res;
     
     printf("Digite o número 1: \n");
-    scanf("%i", &n1);
+    //%i ou %d, mostra pro programa que será impresso na tela um numero
+    //& significa q esse valor será armazenado (vem do usuario)
+    scanf("%i", &n1);    
     
     printf("Digite o número 2: \n");
     scanf("%i", &n2);
     
     res = n1 * n2;
     
-    printf("Resultado: %i", res);
+    printf("Resultado de %i * %i = %i", n1, n2, res);
     return 0;
 }
