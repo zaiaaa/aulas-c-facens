f5daca7f97a194d63591ec1d67beeae6f3f79274
zaiaaa <gustavozaia75@gmail.com>
2025-03-11 22:15:46 -0300

    exercicio de tabela ascii

---
+++ tabela_ascii.cpp
@@ -0,0 +1,13 @@
+/*
+	03 - tabela ascii - Imprimimos char como inteiro, usando %d.
+	Autor: Gustavo Zaia
+	Ultima modificacao: 11/03/2025
+*/
+#include <stdio.h>
+
+int main(){
+	char letra = 'A';
+	
+	printf("Letra %c\n", letra);
+	printf("Tabela ascii: %d", letra);
+}
